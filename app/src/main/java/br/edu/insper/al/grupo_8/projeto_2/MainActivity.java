94262002a527e77e2f8c8cf851f02dadacf3d11d
RogerPina <rogerrfp@al.insper.edu.br>
2019-05-15 16:58:57 -0300

    p

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/MainActivity.java
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/MainActivity.java
@@ -1,13 +1,26 @@
 package br.edu.insper.al.grupo_8.projeto_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void startHomeActivity(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_login = findViewById(R.id.button_login);
+
+        button_login.setOnClickListener((view -> {
+            startHomeActivity();
+        }));
     }
 }
