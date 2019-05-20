7d54f5b30446f45d0879473e2e3726bc07d2dde9
tluigimm <tluigimm@gmail.com>
2019-05-20 16:52:18 -0300

    ...

---
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/BooleanQuestion.java
@@ -0,0 +1,11 @@
+package br.edu.insper.al.grupo_8.projeto_2;
+
+public class BooleanQuestion extends Question{
+    private boolean resposta;
+    private int NumQuestions;
+
+    public BooleanQuestion(int NumQuestion){
+        this.NumQuestions = NumQuestion;
+
+    }
+}
