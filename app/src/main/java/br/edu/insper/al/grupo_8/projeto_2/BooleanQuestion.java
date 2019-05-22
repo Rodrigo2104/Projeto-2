01bcaae9f436a998c7d79bc6446ca83cf0efa8f7
tluigimm <tluigimm@gmail.com>
2019-05-22 14:51:47 -0300

    question boolean

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/BooleanQuestion.java
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/BooleanQuestion.java
@@ -1,11 +1,17 @@
 package br.edu.insper.al.grupo_8.projeto_2;
 
-public class BooleanQuestion extends Question{
+public class BooleanQuestion extends Question {
     private boolean resposta;
-    private int NumQuestions;
 
-    public BooleanQuestion(int NumQuestion){
-        this.NumQuestions = NumQuestion;
+    public BooleanQuestion(String question) {
+        super(question);
+    }
 
+    public void setResposta(boolean resposta){
+        this.resposta = resposta;
+    }
+
+    public boolean getResposta(){
+        return resposta;
     }
 }
