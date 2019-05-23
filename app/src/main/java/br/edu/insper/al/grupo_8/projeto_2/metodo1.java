4490e880602e6e8339f3253b0d056a937715a592
luvi01 <38668635+luvi01@users.noreply.github.com>
2019-05-22 23:13:39 -0300

    Metodo1-Front

---
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/metodo1.java
@@ -0,0 +1,16 @@
+package br.edu.insper.al.grupo_8.projeto_2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class metodo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.metodo1);
+    }
+
+
+
+}
