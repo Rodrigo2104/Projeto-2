b268a9888e5c5c68e87e5e021a85d0cb245ba8b0
RogerPina <rogerrfp@al.insper.edu.br>
2019-05-31 10:14:00 -0300

    resolvendo os CB

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/Methods/MethodVActivity.java
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/Methods/MethodVActivity.java
@@ -5,92 +5,97 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import br.edu.insper.al.grupo_8.projeto_2.R;
 
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class MethodVActivity extends AppCompatActivity{
 
     private int soma;
 
-    private LinkedList<CheckBox> boxes;
+    private HashMap<String, CheckBox> boxes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_method_v);
 
-        boxes = new LinkedList<CheckBox>();
+        boxes = new HashMap<String, CheckBox>();
 
         // === TESTE A ===
 
         CheckBox cb1TestA = findViewById(R.id.cb_1_testA);
-        boxes.add(cb1TestA);
+        boxes.put("A", cb1TestA);
         CheckBox cb2TestA = findViewById(R.id.cb_2_testA);
-        boxes.add(cb2TestA);
+        boxes.put("A", cb2TestA);
         CheckBox cb3TestA = findViewById(R.id.cb_3_testA);
-        boxes.add(cb3TestA);
+        boxes.put("A", cb3TestA);
 
         // ==== TESTE B ===
 
         CheckBox cb1TestB = findViewById(R.id.cb_1_testB);
-        boxes.add(cb1TestB);
+        boxes.put("B", cb1TestB);
         CheckBox cb2TestB = findViewById(R.id.cb_2_testB);
-        boxes.add(cb2TestB);
+        boxes.put("B", cb2TestB);
         CheckBox cb3TestB = findViewById(R.id.cb_3_testB);
-        boxes.add(cb3TestB);
+        boxes.put("B", cb3TestB);
 
         // === TESTE C ===
 
         CheckBox cb1TestC = findViewById(R.id.cb_1_testC);
-        boxes.add(cb1TestC);
+        boxes.put("C", cb1TestC);
         CheckBox cb2TestC = findViewById(R.id.cb_2_testC);
-        boxes.add(cb2TestC);
+        boxes.put("C", cb2TestC);
         CheckBox cb3TestC = findViewById(R.id.cb_3_testC);
-        boxes.add(cb3TestC);
+        boxes.put("C", cb3TestC);
 
         // === TESTE D ===
 
         CheckBox cb1TestD = findViewById(R.id.cb_1_testD);
-        boxes.add(cb1TestD);
+        boxes.put("D", cb1TestD);
         CheckBox cb2TestD = findViewById(R.id.cb_2_testD);
-        boxes.add(cb2TestD);
+        boxes.put("D", cb2TestD);
         CheckBox cb3TestD = findViewById(R.id.cb_3_testD);
-        boxes.add(cb3TestD);
+        boxes.put("D", cb3TestD);
 
         // === TESTE E ===
 
         CheckBox cb1TestE = findViewById(R.id.cb_1_testE);
-        boxes.add(cb1TestE);
+        boxes.put("E", cb1TestE);
         CheckBox cb2TestE = findViewById(R.id.cb_2_testE);
-        boxes.add(cb2TestE);
+        boxes.put("E", cb2TestE);
         CheckBox cb3TestE = findViewById(R.id.cb_3_testE);
-        boxes.add(cb3TestE);
+        boxes.put("E", cb3TestE);
 
         // === TESTE F ===
 
         CheckBox cb1TestF = findViewById(R.id.cb_1_testF);
-        boxes.add(cb1TestF);
+        boxes.put("F", cb1TestF);
         CheckBox cb2TestF = findViewById(R.id.cb_2_testF);
-        boxes.add(cb2TestF);
+        boxes.put("F", cb2TestF);
         CheckBox cb3TestF = findViewById(R.id.cb_3_testF);
-        boxes.add(cb3TestF);
+        boxes.put("F", cb3TestF);
 
         // === TESTE G ===
 
-        CheckBox cb1TestG = findViewById(R.id.cb_1_testG);
-        boxes.add(cb1TestG);
-        CheckBox cb2TestG = findViewById(R.id.cb_2_testG);
-        boxes.add(cb2TestG);
-        CheckBox cb3TestG = findViewById(R.id.cb_3_testG);
-        boxes.add(cb3TestG);
+        HashMap<String, RadioButton> rb = new HashMap<String, RadioButton>();
+
+        RadioButton cb1TestG = findViewById(R.id.cb_1_testG);
+        rb.put("G", cb1TestG);
+        RadioButton cb2TestG = findViewById(R.id.cb_2_testG);
+        rb.put("G", cb2TestG);
+        RadioButton cb3TestG = findViewById(R.id.cb_3_testG);
+        rb.put("G", cb3TestG);
 
         TextView resultSoma = findViewById(R.id.result);
 
-        for(CheckBox box : boxes){
+        for(RadioButton box : rb.values()){
             box.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
