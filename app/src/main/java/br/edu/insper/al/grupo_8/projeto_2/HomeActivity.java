7cf86c634b1523f981084efc4f6c267a70cf0c71
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-06-01 04:06:43 -0300

    Login e FIrebase

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/HomeActivity.java
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/HomeActivity.java
@@ -3,12 +3,24 @@ package br.edu.insper.al.grupo_8.projeto_2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     private void startMethodsActivity() {
         Intent intent = new Intent(this, MethodsActivity.class);
+        startActivity(intent);
+    }
+
+    private void logOutMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
@@ -18,9 +30,22 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         Button buttonGoMethods = findViewById(R.id.button_goMethods);
+        Button singOut = findViewById(R.id.singOut);
+        TextView email = findViewById(R.id.email);
+        FirebaseAuth authentication = FirebaseAuth.getInstance();
+        FirebaseUser user = authentication.getCurrentUser();
+
+        assert user != null;
+        email.setText(user.getEmail());
+
+        singOut.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            logOutMainActivity();
+        });
 
         buttonGoMethods.setOnClickListener((view -> {
             startMethodsActivity();
         }));
     }
+
 }
