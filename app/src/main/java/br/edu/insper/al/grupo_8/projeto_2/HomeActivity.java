fe8ca341a37fbb98522e24bfd21786ff3e220e46
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-06-05 10:03:46 -0300

    12 horas...

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/HomeActivity.java
+++ app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/HomeActivity.java
@@ -1,19 +1,40 @@
 package br.edu.insper.al.grupo_8.projeto_2;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 
 public class HomeActivity extends AppCompatActivity {
 
-    private void startMethodsActivity() {
-        Intent intent = new Intent(this, MethodsActivity.class);
+    private FirebaseAuth mAuth;
+    private String userID;
+    ListView listView;
+
+    private void startAddActivity() {
+        Intent intent = new Intent(this, AddActivity.class);
         startActivity(intent);
     }
 
@@ -29,23 +50,39 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button buttonGoMethods = findViewById(R.id.button_goMethods);
-        Button singOut = findViewById(R.id.singOut);
-        TextView email = findViewById(R.id.email);
-        FirebaseAuth authentication = FirebaseAuth.getInstance();
-        FirebaseUser user = authentication.getCurrentUser();
+        Button add = findViewById(R.id.add_pacient);
+        add.setOnClickListener((view -> startAddActivity()));
 
-        assert user != null;
-        email.setText(user.getEmail());
+//      =============== Firebase ===============
+        listView = findViewById(R.id.pacients);
 
-        singOut.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            logOutMainActivity();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference pacientesReference = database.getReference("Pacientes");
+
+        pacientesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
-
-        buttonGoMethods.setOnClickListener((view -> {
-            startMethodsActivity();
-        }));
     }
 
+    private void showData(DataSnapshot dataSnapshot) {
+        ArrayList<String> listaPacientes = new ArrayList<>();
+        for (DataSnapshot ds : dataSnapshot.getChildren()){
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaPacientes);
+            Paciente pac = new Paciente();
+            pac.setNome(ds.child("Info").getValue(Paciente.class).getNome());
+            pac.setRh(ds.child("Info").getValue(Paciente.class).getRh());
+            pac.setData_internacao(ds.child("Info").getValue(Paciente.class).getData_internacao());
+            listaPacientes.add(pac.getNome());
+            listView.setAdapter(arrayAdapter);
+
+        }
+    }
 }
