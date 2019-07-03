a39dfb912ad630a5a07f96669b4fec57ed9f6f4b
FidyBack <abel.cavalcante.ismart@gmail.com>
2019-07-03 01:00:58 -0300

    All

--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/Question.java
+++
@@ -1,9 +0,0 @@
-package br.edu.insper.al.grupo_8.projeto_2;
-
-public class Question {
-    private String question;
-
-    public Question(String question) {
-        this.question = question;
-    }
-}
--- app/src/main/java/br/edu/insper/al/grupo_8/projeto_2/QuestionString.java
+++
@@ -1,8 +0,0 @@
-package br.edu.insper.al.grupo_8.projeto_2;
-
-public class QuestionString extends Question {
-
-    public QuestionString(String question) {
-        super(question);
-    }
-}
